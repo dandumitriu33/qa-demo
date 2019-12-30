95a8c869a38d77294baf6b2c48a627bc43b38e46
dandumitriu33 <dandumitriu33@gmail.com>
2019-12-30 08:39:51 +0200

    Change displayed date and time to readable format

--- data_manager.py
+++ data_manager.py
@@ -1,5 +1,6 @@
 import database_common
 import psycopg2
+import datetime
 import time
 
 
@@ -48,7 +49,7 @@ def get_answers_for_question(cursor, question_id):
 
 @database_common.connection_handler
 def post_question(cursor, title, message, image=None):
-    submission_time = int(round(time.time() * 1000))
+    submission_time = datetime.datetime.utcnow().isoformat(' ', 'seconds')
     cursor.execute(f"""
                     INSERT INTO questions (submission_time, view_number, vote_number, title, message, image)
                     VALUES ('{submission_time}', 0, 0, '{title}', '{message}', '{image}');
@@ -120,7 +121,7 @@ def delete_question(cursor, question_id):
 
 @database_common.connection_handler
 def post_answer(cursor, question_id, message, image=None):
-    submission_time = int(round(time.time() * 1000))
+    submission_time = datetime.datetime.utcnow().isoformat(' ', 'seconds')
     cursor.execute(f"""
                     INSERT INTO answers (submission_time, vote_number, question_id, message, image)
                     VALUES ('{submission_time}', 0, {question_id}, '{message}', '{image}')
